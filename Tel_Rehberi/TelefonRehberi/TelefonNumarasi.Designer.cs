c088b2a70fb580690204b6424117fd9b8d77084f
boransen <81160691+boransen@users.noreply.github.com>
2023-12-27 06:02:13 +0300

    Add files via upload

---
+++ Tel_Rehberi/TelefonRehberi/TelefonNumarasi.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TelefonRehberi
+{
+    partial class TelefonNumarasi
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+       
+        /// <param name="disposing"
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxAdSoyad = new System.Windows.Forms.TextBox();
+            this.maskedTextBoxTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxMail = new System.Windows.Forms.TextBox();
+            this.buttonKaydet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name Surname";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Phone No";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 92);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Email";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBoxAdSoyad
+            // 
+            this.textBoxAdSoyad.Location = new System.Drawing.Point(188, 25);
+            this.textBoxAdSoyad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxAdSoyad.Name = "textBoxAdSoyad";
+            this.textBoxAdSoyad.Size = new System.Drawing.Size(380, 22);
+            this.textBoxAdSoyad.TabIndex = 1;
+            // 
+            // maskedTextBoxTelefon
+            // 
+            this.maskedTextBoxTelefon.Location = new System.Drawing.Point(188, 57);
+            this.maskedTextBoxTelefon.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.maskedTextBoxTelefon.Mask = "(999) 000 00 00";
+            this.maskedTextBoxTelefon.Name = "maskedTextBoxTelefon";
+            this.maskedTextBoxTelefon.Size = new System.Drawing.Size(380, 22);
+            this.maskedTextBoxTelefon.TabIndex = 2;
+            // 
+            // textBoxMail
+            // 
+            this.textBoxMail.Location = new System.Drawing.Point(188, 89);
+            this.textBoxMail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxMail.Name = "textBoxMail";
+            this.textBoxMail.Size = new System.Drawing.Size(380, 22);
+            this.textBoxMail.TabIndex = 3;
+            // 
+            // buttonKaydet
+            // 
+            this.buttonKaydet.Location = new System.Drawing.Point(469, 121);
+            this.buttonKaydet.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonKaydet.Name = "buttonKaydet";
+            this.buttonKaydet.Size = new System.Drawing.Size(100, 28);
+            this.buttonKaydet.TabIndex = 4;
+            this.buttonKaydet.Text = "Save";
+            this.buttonKaydet.UseVisualStyleBackColor = true;
+            this.buttonKaydet.Click += new System.EventHandler(this.buttonKaydet_Click);
+            // 
+            // TelefonNumarasi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 164);
+            this.Controls.Add(this.buttonKaydet);
+            this.Controls.Add(this.textBoxMail);
+            this.Controls.Add(this.maskedTextBoxTelefon);
+            this.Controls.Add(this.textBoxAdSoyad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "TelefonNumarasi";
+            this.Text = "TelefonNumarasi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxAdSoyad;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxTelefon;
+        private System.Windows.Forms.TextBox textBoxMail;
+        private System.Windows.Forms.Button buttonKaydet;
+    }
+}
